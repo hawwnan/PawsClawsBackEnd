222f68e110d2ff81ca9ae79cd841b9a5a2f1b8b0
Muhammad Hannan <130058839+hawwnan@users.noreply.github.com>
2023-07-05 14:30:20 +0500

    Delete urls directory

--- urls/lfs_urls.py
+++
@@ -1,9 +0,0 @@
-#connecting views with urls
-from django.urls import path
-from back.views import lfs_views as views
-
-
-
-urlpatterns = [
-    path('mylfs/', views.getMyLfs, name ='my-orders'),
-]
