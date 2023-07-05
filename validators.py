e9db94e78d67eb9a96d480e241d5b30a59f35865
Muhammad Hannan <130058839+hawwnan@users.noreply.github.com>
2023-07-05 14:33:56 +0500

    Delete validators.py

--- validators.py
+++
@@ -1,9 +0,0 @@
-import re
-from django.core.exceptions import ValidationError
-from django.contrib.auth.models import User
-
-def validate_email(email):
-    if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
-        raise ValidationError("Enter a valid email address.")
-    if User.objects.filter(email=email).exists():
-        raise ValidationError("A user with that email address already exists.")
